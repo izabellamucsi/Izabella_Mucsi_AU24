92dec050a7bf68fa27f50419681fa5d5bf7cf85e
izabellamucsi <izabellamucsi@gmail.com>
2024-11-11 22:36:26 +0100

    Add files via upload

--- SQL_DML_Izabella_Mucsi_HW_Task1.sql
+++ SQL_DML_Izabella_Mucsi_HW_Task1.sql
@@ -4,7 +4,7 @@
 /* 1.1 Choose your top-3 favorite movies and add them to the 'film' table  */
 /* 1.2 Fill in rental rates with 4.99, 9.99 and 19.99 and rental durations with 1, 2 and 3 weeks respectively. */
 /* 1.3 Add the actors who play leading roles in your favorite movies to the 'actor' and 'film_actor' tables (6 or more actors in total).  */ */
-  
+  SELECT * FROM film f ORDER BY film_id
 --first favorite movie 
 
 WITH defined_rental_specification AS (
@@ -15,28 +15,40 @@ SELECT 4.99 AS rental_rate,
     'RUSH' AS title,
      'A biographical sports drama film about the rivalry between Formula One drivers Niki Lauda and James Hunt' AS description,
      2013 AS release_year,
-     1 AS language_id,
-     1 AS original_language_id,
      123 AS "length",
      19.99 AS replacement_cost,
-     'R'::mpaa_rating AS rating,
       now() AS last_update ),
+      	language_data as(
+      	SELECT l.language_id AS language_id
+      	FROM public."language" l
+      	WHERE lower(l.name) = 'english'),
+      		original_language_data AS (
+      		SELECT l.language_id AS  original_language_id
+      		FROM public."language" l 
+      		WHERE lower(l.name) = 'english'),
+      			rating_data AS (
+      			SELECT f.rating
+      			FROM public.film f
+      			WHERE f.film_id = 8),
 		insert_first_movie AS (																			-- Inserting the movie if it doesn't already exist in the `film` table
  		INSERT INTO public.film (title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update)
     	SELECT 
         ffm.title,
         ffm.description,
         ffm.release_year,
-        ffm.language_id,
-        ffm.original_language_id,
+        ld.language_id,
+        old.original_language_id,
         drs.rental_duration,     -- Using rental duration from defined rental specification
         drs.rental_rate,         -- Using rental rate from defined rental specification
         ffm.length,
         ffm.replacement_cost,
-        ffm.rating,
+        rd.rating,
         ffm.last_update
     	FROM first_favorite_movie ffm
     	CROSS JOIN defined_rental_specification drs  -- Joining rental specification
+    	CROSS JOIN language_data ld					 -- Joining language data 
+    	CROSS JOIN rating_data rd					 -- Joining rating Data
+    	CROSS JOIN original_language_data OLD		 -- Joining original language data
     	WHERE NOT EXISTS (
         SELECT * 
         FROM public.film f 
@@ -68,7 +80,7 @@ SELECT 4.99 AS rental_rate,
 										FROM insert_first_movie ifm
 										CROSS JOIN insert_actor ia
 										RETURNING film_id, actor_id;
-
+										COMMIT;
 --second favorite movie 
 
 WITH defined_rental_specification AS (
@@ -79,28 +91,40 @@ SELECT 9.99 AS rental_rate,
     'SENNA' 			  AS title,
      'A documentary film about the life and career of Brazilian Formula One racing driver Ayrton Senna' AS description,
      2010 				  AS release_year,
-	 1 					  AS language_id,
-	 1 					  AS original_language_id,
 	 106 				  AS "length",
 	 19.99 				  AS replacement_cost,
-	 'PG-13'::mpaa_rating AS rating,
 	 now() 				  AS last_update),
+	 	language_data as(
+      	SELECT l.language_id AS language_id
+      	FROM public."language" l
+      	WHERE lower(l.name) = 'english'),
+      		original_language_data AS (
+      		SELECT l.language_id AS  original_language_id
+      		FROM public."language" l 
+      		WHERE lower(l.name) = 'english'),
+      			rating_data AS (
+      			SELECT f.rating
+      			FROM public.film f
+      			WHERE f.film_id = 7),
 		insert_first_movie AS (																			-- Inserting the movie if it doesn't already exist in the `film` table
  		INSERT INTO public.film (title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update)
     	SELECT 
         ffm.title,
         ffm.description,
         ffm.release_year,
-        ffm.language_id,
-        ffm.original_language_id,
-        drs.rental_duration,     -- Using rental duration from defined rental specification
-        drs.rental_rate,         -- Using rental rate from defined rental specification
+        ld.language_id,
+        old.original_language_id, 
+        drs.rental_duration,      
+        drs.rental_rate,         
         ffm.length,
         ffm.replacement_cost,
-        ffm.rating,
+        rd.rating,
         ffm.last_update
     	FROM first_favorite_movie ffm
     	CROSS JOIN defined_rental_specification drs  -- Joining rental specification
+    	CROSS JOIN language_data ld					 -- Joining language data 
+    	CROSS JOIN rating_data rd					 -- Joining rating Data
+    	CROSS JOIN original_language_data OLD		 -- Joining original language data
     	WHERE NOT EXISTS (
         SELECT * 
         FROM public.film f 
@@ -132,7 +156,7 @@ SELECT 9.99 AS rental_rate,
 										FROM insert_first_movie ifm
 										CROSS JOIN insert_actor ia
 										RETURNING film_id, actor_id;
-
+										COMMIT;
 --third favorite movie 
 
 WITH defined_rental_specification AS (
@@ -143,28 +167,40 @@ SELECT 19.99 AS rental_rate,
     'DRIVE TO SURVIVE'  AS title,
 	 'A documentary that follows Formula One teams and drivers during a Formula One season' AS description,
 	 2019 			    AS release_year,
-	 1 				    AS language_id,
-	 1 				    AS original_language_id,
 	 45 				AS "length",
 	 19.99 			    AS replacement_cost,
-	 'G'::mpaa_rating   AS rating,
 	 now() AS last_update),
+	 	language_data as(
+      	SELECT l.language_id AS language_id
+      	FROM public."language" l
+      	WHERE lower(l.name) = 'english'),
+      		original_language_data AS (
+      		SELECT l.language_id AS  original_language_id
+      		FROM public."language" l 
+      		WHERE lower(l.name) = 'english'),
+      			rating_data AS (
+      			SELECT f.rating
+      			FROM public.film f
+      			WHERE f.film_id = 2),
 		insert_first_movie AS (																			-- Inserting the movie if it doesn't already exist in the `film` table
  		INSERT INTO public.film (title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update)
     	SELECT 
         ffm.title,
         ffm.description,
         ffm.release_year,
-        ffm.language_id,
-        ffm.original_language_id,
-        drs.rental_duration,     -- Using rental duration from defined rental specification
-        drs.rental_rate,         -- Using rental rate from defined rental specification
+        ld.language_id,
+        old.original_language_id,
+        drs.rental_duration,     
+        drs.rental_rate,         
         ffm.length,
         ffm.replacement_cost,
-        ffm.rating,
+        rd.rating,
         ffm.last_update
     	FROM first_favorite_movie ffm
     	CROSS JOIN defined_rental_specification drs  -- Joining rental specification
+    	CROSS JOIN language_data ld					 -- Joining language data 
+    	CROSS JOIN rating_data rd					 -- Joining rating Data
+    	CROSS JOIN original_language_data OLD		 -- Joining original language data
     	WHERE NOT EXISTS (
         SELECT * 
         FROM public.film f 
@@ -196,7 +232,7 @@ SELECT 19.99 AS rental_rate,
 										FROM insert_first_movie ifm
 										CROSS JOIN insert_actor ia
 										RETURNING film_id, actor_id;
-
+										COMMIT;
 
 
 /* 1.1b -  Choose your top-3 favorite movies and add them to the 'film' table (films with the title Film1, Film2, etc - will not be taken into account and grade will be reduced) */
@@ -206,39 +242,39 @@ SELECT
 'RUSH' 				AS title,
 'A biographical sports drama film about the rivalry between Formula One drivers Niki Lauda and James Hunt' AS description,
 2013 				AS release_year,
-1 					AS language_id,
-1 					AS original_language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS original_language_id,
 3 					AS rental_duration,
 4.99 				AS rental_rate,
 123 				AS "length",
 19.99 				AS replacement_cost,
-'R'::mpaa_rating 	AS rating,
+(SELECT f.rating FROM public.film f WHERE f.film_id = 8) AS rating,
 now() 			 	AS last_update
 UNION ALL
 SELECT 
 'SENNA' 				AS title,
 'A documentary film about the life and career of Brazilian Formula One racing driver Ayrton Senna' AS description,
 2010 					AS release_year,
-1 						AS language_id,
-1 						AS original_language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS original_language_id,
 3 						AS rental_duration,
 3.99 					AS rental_rate,
 106 					AS "length",
 19.99 					AS replacement_cost,
-'PG-13'::mpaa_rating 	AS rating,
+(SELECT f.rating FROM public.film f WHERE f.film_id = 7) AS rating,
 now() 					AS last_update
 UNION ALL
 SELECT 
 'DRIVE TO SURVIVE' 	AS title,
 'A documentary that follows Formula One teams and drivers during a Formula One season' AS description,
 2019 				AS release_year,
-1 					AS language_id,
-1 					AS original_language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS language_id,
+(SELECT l.language_id FROM public."language" l WHERE lower(l.name) = 'english') AS original_language_id,
 3 					AS rental_duration,
 2.99 				AS rental_rate,
 45 					AS "length",
 19.99 				AS replacement_cost,
-'G'::mpaa_rating 	AS rating,
+(SELECT f.rating FROM public.film f WHERE f.film_id = 2) AS rating,
 now() 				AS last_update),
 	inserted_movies AS (
 	INSERT INTO film (title,description,release_year,language_id,original_language_id,rental_duration,rental_rate,length,replacement_cost,rating,last_update) --Inserting the movies into the film table
@@ -250,25 +286,28 @@ now() 				AS last_update),
 	WHERE f.title = fm.title)
 	RETURNING film_id)
 	SELECT * FROM inserted_movies;
-
+	COMMIT;
 
 /*1.2 b - Fill in rental rates with 4.99, 9.99 and 19.99 and rental durations with 1, 2 and 3 weeks respectively.*/
 
-WITH insert_rental_data AS (                              --Updating the rental data for the inserted favorite films
-    UPDATE public.film
-    SET 
-    rental_rate = CASE
-    WHEN upper(title) = 'RUSH' THEN 4.99
-    WHEN upper(title) = 'SENNA' THEN 9.99
-    WHEN upper(title) = 'DRIVE TO SURVIVE' THEN 12.99    
-    END,
-    rental_duration = CASE
-    WHEN upper(title) = 'RUSH' THEN 1
-    WHEN upper(title) = 'SENNA' THEN 2
-    WHEN upper(title) = 'DRIVE TO SURVIVE' THEN 3
-    END
-    WHERE upper(title) IN ('RUSH', 'SENNA', 'DRIVE TO SURVIVE')
-    RETURNING rental_rate, rental_duration
+UPDATE public.film						 --Updating the rental data for the inserted favorite films
+SET 
+   rental_rate = CASE
+       WHEN upper(title) = 'RUSH' THEN 4.99
+       WHEN upper(title) = 'SENNA' THEN 9.99
+       WHEN upper(title) = 'DRIVE TO SURVIVE' THEN 12.99    
+   END,
+   rental_duration = CASE
+       WHEN upper(title) = 'RUSH' THEN 1
+       WHEN upper(title) = 'SENNA' THEN 2
+       WHEN upper(title) = 'DRIVE TO SURVIVE' THEN 3
+   END
+WHERE upper(title) IN ('RUSH', 'SENNA', 'DRIVE TO SURVIVE')
+RETURNING rental_rate, rental_duration;
+
+COMMIT;
+   
+
 
 /* 1.3b - Add the actors who play leading roles in your favorite movies to the 'actor' and 'film_actor' tables (6 or more actors in total). */
 
@@ -313,8 +352,9 @@ SELECT 'ALAIN' AS first_name, 'PROST' AS last_name
 			film_id
 			FROM film_ids
 			WHERE film_id IS NOT NULL; 
+			COMMIT;
 
-
+	
 /* 1.4 Add your favorite movies to any store's inventory 
  * In the following query I have added the favorite movies to the inventory table using CTE*/
  
@@ -323,16 +363,22 @@ SELECT
 f.film_id AS movie_id
 FROM public.film f 
 WHERE UPPER(f.title) IN ('RUSH', 'SENNA','DRIVE TO SURVIVE')),
+	chosen_store AS (
+	 SELECT s.store_id AS store_id
+	 FROM public.staff s
+	 WHERE s.staff_id = 1
+	 LIMIT 1),
 	insert_inventory AS (
 	INSERT INTO public.inventory (film_id, store_id, last_update)								--Inserting the selected movies into the inventory table
 	SELECT 
 	im.movie_id,
-	1 AS store_id,
+	cs.store_id,
 	current_date
 	FROM inserted_movies im
+	CROSS JOIN chosen_store cs
 	RETURNING inventory_id)
 	SELECT * FROM insert_inventory; 
-
+	COMMIT;
 /* 1.5 Alter any existing customer in the database with at least 43 rental and 43 payment records. Change their personal data to yours (first name, last name, address, etc.). 
  * I the next query I have found an existing customer with at least 43 rentals and payments in the table chosen_customer with the customer_id 401 and then updated their personal information with my details.*/
 
@@ -350,7 +396,11 @@ LIMIT 1),
 		updated_customer AS (																			-- Updating the personal data of the selected customer
 		UPDATE public.customer 
 		SET 		
-		store_id= 1,
+		store_id= 
+				(SELECT s.store_id AS store_id
+	 			FROM public.staff s
+	 			WHERE s.staff_id = 1
+	 			LIMIT 1),
 		first_name='Izabella', 
 		last_name='Mucsi', 
 		email='izabellamucsi@gmail.com',
@@ -368,9 +418,7 @@ LIMIT 1),
 											FROM chosen_customer cc)
 											RETURNING customer_id)
 											SELECT * FROM updated_customer;
-
-
-
+											COMMIT;
 
 
 /* 1.6 Remove any records related to you (as a customer) from all tables except 'Customer' and 'Inventory'
@@ -397,7 +445,7 @@ WHERE LOWER(c.first_name) = 'izabella' AND LOWER(last_name) = 'mucsi'))
 	WHERE LOWER(c.first_name) = 'izabella' AND LOWER(last_name) = 'mucsi'))
 	RETURNING rental_id)
 	SELECT * FROM rental_delete;
-
+	COMMIT;
  
 /* 1.7 Rent your favorite movies from the store they are in and pay for them (add corresponding records to the database to represent this activity)
  * In the following query the previously specified customer will rent out the three favorite movies from the first tak by inserting the appropriate records 
@@ -420,32 +468,40 @@ WHERE UPPER(f.title) IN ('SENNA', 'RUSH','DRIVE TO SURVIVE')
 ),
 		rental_date AS (
 		SELECT CAST('2017-01-01' AS date) AS rental_date
-		),																		-- Inserting the rental records for each movie 
-			rental_insert AS (
-			INSERT INTO public.rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
-			SELECT 
-			rd.rental_date,
-			rm.inventory_id,
-			rc.customer,
-			rd.rental_date + (rm.duration * INTERVAL '1 week') AS return_date,
-			1 AS staff_id,
-			now() AS last_update
-			FROM renting_movies rm
-			CROSS JOIN renting_customer rc
-			CROSS JOIN rental_date rd
-			RETURNING rental_id, inventory_id),
-				payment_insert AS (													-- Inserting the payment records for each rental 
-				INSERT INTO public.payment (customer_id, staff_id, rental_id, amount, payment_date)
+		),	
+			staff_data AS (
+			SELECT s.staff_id AS staff_id
+			FROM public.staff s
+			WHERE lower(s.username) = 'mike'),
+				rental_insert AS (												-- Inserting the rental records for each movie 
+				INSERT INTO public.rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
 				SELECT 
+				rd.rental_date,
+				rm.inventory_id,
 				rc.customer,
-				1 AS staff_id,
-				ri.rental_id,
-				rm.rate * rm.duration AS amount,
-				rd.rental_date 
-				FROM rental_insert ri
+				rd.rental_date + (rm.duration * INTERVAL '1 week') AS return_date,
+				sd.staff_id,
+				now() AS last_update
+				FROM renting_movies rm
 				CROSS JOIN renting_customer rc
 				CROSS JOIN rental_date rd
-				INNER JOIN renting_movies rm ON rm.inventory_id = ri.inventory_id
-				RETURNING payment_id)
- 				SELECT * FROM payment_insert,rental_insert;
- 
+				CROSS JOIN staff_data sd
+				RETURNING rental_id, inventory_id),
+					payment_insert AS (													-- Inserting the payment records for each rental 
+					INSERT INTO public.payment (customer_id, staff_id, rental_id, amount, payment_date)
+					SELECT 
+					rc.customer,
+					sd.staff_id,
+					ri.rental_id,
+					rm.rate * rm.duration AS amount,
+					rd.rental_date 
+					FROM rental_insert ri
+					CROSS JOIN renting_customer rc
+					CROSS JOIN rental_date rd
+					CROSS JOIN staff_data sd
+					INNER JOIN renting_movies rm ON rm.inventory_id = ri.inventory_id
+					RETURNING payment_id)
+	 				SELECT * FROM payment_insert,rental_insert;
+	 				COMMIT;
+
+	 			
