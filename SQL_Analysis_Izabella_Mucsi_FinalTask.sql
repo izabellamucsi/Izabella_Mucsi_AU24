bbec41007cffd82a2e3e4d9e727f53b897ca2605
izabellamucsi <izabellamucsi@gmail.com>
2024-12-21 18:10:46 +0100

    Add files via upload

--- SQL_Analysis_Izabella_Mucsi_FinalTask.sql
+++ SQL_Analysis_Izabella_Mucsi_FinalTask.sql
@@ -57,7 +57,7 @@ GROUP BY p.prod_subcategory_id,p.prod_subcategory, EXTRACT(YEAR FROM s.time_id))
 	HAVING COUNT(DISTINCT sales_year) = 4;																
 
 /*Task 3.reate a query to generate a sales report for the years 1999 and 2000, focusing on quarters and product categories. In the report you have to  analyze the sales of products from the categories 'Electronics,' 'Hardware,' and 'Software/Other,' across the distribution channels 'Partners' and 'Internet'.
- * Solution: The query filters the data to include only the specified years, product categories, and distribution channels in order to calculate the cumulative sum of sales by quarter and the total sales for each product category for each quarter.
+ * Solution A: The query filters the data to include only the specified years, product categories, and distribution channels in order to calculate the cumulative sum of sales by quarter and the total sales for each product category for each quarter.
  * In order to calculate the percentage by which sales increased or decreased compared to the first quarter of the year the FIRST_VALUE function was used which retrieves the sales amount from the first quarter for each product category. The diff_percent column shows the difference between the current quarter's sales and the first quarter's sales.*/
 
 SELECT 
@@ -70,6 +70,27 @@ CASE
 	ELSE ROUND((SUM(s.amount_sold) - FIRST_VALUE(SUM(s.amount_sold)) OVER (PARTITION BY EXTRACT(YEAR FROM s.time_id),p.prod_category ORDER BY EXTRACT(QUARTER FROM s.time_id))) * 100.0 /  FIRST_VALUE(SUM(s.amount_sold)) OVER ( PARTITION BY EXTRACT(YEAR FROM s.time_id), p.prod_category ORDER BY EXTRACT(QUARTER FROM s.time_id)), 2 ) || '%'    --Calculating the percentage difference compared to Q1 sales
 END AS diff_percent,
 ROUND(SUM(SUM(s.amount_sold)) OVER (PARTITION BY EXTRACT(YEAR FROM s.time_id), EXTRACT(quarter FROM s.time_id) ORDER BY EXTRACT(QUARTER FROM s.time_id)), 2) AS cum_sum$   																																											  --Cumulative sum of sales for the quarters
+FROM sh.sales s 
+INNER JOIN sh.products p ON s.prod_id = p.prod_id
+INNER JOIN sh.channels c ON s.channel_id = c.channel_id
+WHERE EXTRACT(YEAR FROM s.time_id) BETWEEN 1999 AND 2000
+AND lower(p.prod_category) IN ('electronics', 'hardware', 'software/other')
+AND lower(c.channel_desc) IN ('partners', 'internet')
+GROUP BY EXTRACT(YEAR FROM s.time_id),EXTRACT(QUARTER FROM s.time_id), p.prod_category
+ORDER BY calendar_year,calendar_quarter_desc,sales$ DESC;
+
+/*Solution B: The query uses WINDOW frames with RANGE mode to define the cumuative sum values. As the task required time based grouping the range mode was appropriate for calculating cumulative sums and percentage differences based quarters and years. */
+
+SELECT 
+EXTRACT(YEAR FROM s.time_id) AS calendar_year,
+concat(EXTRACT(YEAR FROM s.time_id),'-',(lpad(EXTRACT(QUARTER FROM s.time_id)::text,2,'0'))) AS calendar_quarter_desc,
+p.prod_category AS prod_category,
+ROUND(SUM(s.amount_sold), 2) AS sales$,
+CASE 
+	WHEN EXTRACT(QUARTER FROM s.time_id) = 1 THEN 'N/A'
+	ELSE ROUND((SUM(s.amount_sold) - FIRST_VALUE(SUM(s.amount_sold)) OVER (PARTITION BY EXTRACT(YEAR FROM s.time_id), prod_category ORDER BY EXTRACT(QUARTER FROM s.time_id) RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) * 100 /  FIRST_VALUE(SUM(s.amount_sold)) OVER (PARTITION BY EXTRACT(YEAR FROM s.time_id), prod_category ORDER BY EXTRACT(QUARTER FROM s.time_id) RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING), 2 ) || '%'        
+END AS diff_percent,
+SUM(SUM(s.amount_sold)) OVER (PARTITION BY EXTRACT(YEAR FROM s.time_id), EXTRACT(quarter FROM s.time_id) ORDER BY EXTRACT(YEAR FROM s.time_id) RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cum_sum$   																																											
 FROM sh.sales s 
 INNER JOIN sh.products p ON s.prod_id = p.prod_id
 INNER JOIN sh.channels c ON s.channel_id = c.channel_id
